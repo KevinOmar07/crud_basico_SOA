e06474814ca1bc91f17c9faef41bf887c2cf471d
Kevin O <Kevinnuricumbo@gmail.com>
2022-09-21 20:19:53 -0500

    First commit

---
+++ db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    HOST: "bvyy42n8sjqjs3j457rf-mysql.services.clever-cloud.com",
+    USER: "ulls4iwpmssdl27g",
+    PASSWORD: "PduXg4FtvBoQUGG6myD7",
+    DB: "bvyy42n8sjqjs3j457rf",
+    dialect: "mysql"
+}
