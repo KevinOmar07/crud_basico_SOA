d1a923cab9c97324bff3b95127b3d15a0e9e1b1f
KevinOmar07 <Kevinnuricumbo@gmail.com>
2022-09-22 11:24:40 -0500

    Conexion a bd en la nube

--- db.config.js
+++ db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: "bvyy42n8sjqjs3j457rf-mysql.services.clever-cloud.com",
-    USER: "ulls4iwpmssdl27g",
-    PASSWORD: "PduXg4FtvBoQUGG6myD7",
-    DB: "bvyy42n8sjqjs3j457rf",
+    HOST: "54.242.220.9",
+    USER: "kevinomar",
+    PASSWORD: "password",
+    DB: "crudsoa",
     dialect: "mysql"
 }
